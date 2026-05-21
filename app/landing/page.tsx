2b3e9777a40a8957a7f5273d968b9571b7b48dc4
Anar Nyambayar <an301@duke.edu>
2026-05-20 22:10:46 -0400

    updated construction sign

--- app/landing/page.tsx
+++ app/landing/page.tsx
@@ -1,5 +1,17 @@
-import { redirect } from "next/navigation";
+"use client";
 
-export default function LandingRedirect() {
-  redirect("/login");
+import Link from "next/link";
+import {Button} from "@/components/ui/button";
+import { useLanguage } from "@/components/ui/language-provider";
+
+export default function ConstructionPage() {
+  const {t} = useLanguage();
+  return (
+    <div clasName="text-center flex flex-col justify-center py-10 space-y-4">
+    <h3 clasName="text-2xl">
+    ChatDKU is under construciton for the time being. Sorry for the inconvenience!
+      </h3>
+    <Link href="/"><Button>Home</Button></Link>
+    </div>
+  )
 }
